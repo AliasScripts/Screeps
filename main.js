86a491decb85172ca6d64d806ce9d017d3783467
AliasScripts <rktpins@gmail.com>
2017-02-21 19:14:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-var roleRepair = require('role.repair');
+//var roleRepair = require('role.repair');
 var passiveRoads = require('passive.roads');
 
 module.exports.loop = function () {
